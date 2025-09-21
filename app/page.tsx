2fb11c5143ddd19bb771b852a7f26a9569b0722d
Krishna Nagar <notkrises@gmail.com>
2025-09-20 21:53:29 -0400

    Creating routing and navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
+      <Link href="/users">Users</Link>
+
       <Button>pog</Button>
     </main>
   );
---
+++ app/users/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+//page.tsx files are conventions which next.js looks for; thus is the only file than can be seen as a page
+const UsersPage = () => {
+  return <div>UsersPage</div>;
+};
+
+export default UsersPage;

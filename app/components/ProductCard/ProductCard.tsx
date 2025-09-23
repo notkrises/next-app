e4dce30790f6af625283b67baa5298b9d061da35
Krishna Nagar <notkrises@gmail.com>
2025-09-22 21:14:10 -0400

    Playing around with Tailwind CSS

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const ProductCard = () => {
   return (
-    <div className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-600">
+    <div className="p-5 my-5 bg-red-900 text-white text-xl border-4 border-black hover:border-b-gray-950 hover:bg-fuchsia-950">
       <AddToCart />
     </div>
   );

d84e842aa60495a9bf4e8dc8129b244ee174beed
Krishna Nagar <notkrises@gmail.com>
2025-09-24 22:49:42 -0400

    Accessing Query String Parameters

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -6,7 +6,9 @@ import {
   TableBody,
   Table,
 } from "@/components/ui/table";
+import Link from "next/link";
 import React from "react";
+import { sort } from "fast-sort";
 
 interface User {
   id: number;
@@ -14,20 +16,32 @@ interface User {
   email: string;
 }
 
-const UserTable = async () => {
+interface Props {
+  sortOrder: string;
+}
+
+const UserTable = async ({ sortOrder }: Props) => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await res.json();
+
+  const sortedUsers = sort(users).asc(
+    sortOrder === "email" ? (user) => user.email : (user) => user.name
+  );
 
   return (
     <Table>
       <TableHeader>
         <TableRow>
-          <TableHead className="w-[100px]">Name</TableHead>
-          <TableHead>Email</TableHead>
+          <TableHead className="w-[100px]">
+            <Link href="/users?sortOrder=name">Name</Link>
+          </TableHead>
+          <TableHead>
+            <Link href="/users?sortOrder=email">Email</Link>
+          </TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {users.map((user) => (
+        {sortedUsers.map((user) => (
           <TableRow key={user.id}>
             <TableCell>{user.name}</TableCell>
             <TableCell>{user.email}</TableCell>
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import UserTable from "./UserTable";
 
+interface Props {
+  searchParams: { sortOrder: string };
+}
+
 //page.tsx files are conventions which next.js looks for; thus is the only file than can be seen as a page
-const UsersPage = async () => {
+const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <>
       <h1>Users</h1>
-      <UserTable />
+      <UserTable sortOrder={sortOrder} />
     </>
   );
 };

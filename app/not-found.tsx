ae71a9bfaf858988ba9bebdab8577e6516c550d3
Krishna Nagar <notkrises@gmail.com>
2025-09-26 21:33:26 -0400

    Handling Not Found Errors

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return <div>How the pog did you get here bro? This shit don't exist.</div>;
+};
+
+export default NotFoundPage;
---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserNotFoundPage = () => {
+  return <div>This pogging user is not the pog.</div>;
+};
+
+export default UserNotFoundPage;

427434be80a21bdecc034fd01bb5a27e905e632d
Krishna Nagar <notkrises@gmail.com>
2025-09-28 21:21:14 -0400

    Updating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -8,3 +8,24 @@ export function GET(
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: "Mosh" });
 }
+
+// PUT is used for replacting an object
+// PATCH is used for updating one or more properties
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Validate the request body
+  const body = await request.json();
+  // If invalid, return 400
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  // Fetch the user with the given id
+
+  // IF doesn't exist, return 404
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  // Update the user
+  // Return the updated user
+  return NextResponse.json({ id: 1, name: body.name });
+}

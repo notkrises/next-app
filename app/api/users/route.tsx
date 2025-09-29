190b413b1842f0223c1ce242bcea43d2dd06d10c
Krishna Nagar <notkrises@gmail.com>
2025-09-28 21:09:38 -0400

    Creating an Object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,8 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// this is how you create an endpoint for returning a collection of objects
 export function GET(request: NextRequest) {
   return NextResponse.json([
     { id: 1, name: "Mosh" },
     { id: 2, name: "Josh" },
   ]);
-} // this is how you create an endpoint for returning a collection of objects
+}
+
+//POST handles post requests which are used for creating objects
+export async function POST(request: NextRequest) {
+  //  Validate
+  const body = await request.json();
+  // If invalid, return 400 error
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  // Else, return
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 }); //common practice to set status to 201 when a new object is created
+}

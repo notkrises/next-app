81745077ed027bf25d48099c7fea9dc97d563e86
Krishna Nagar <notkrises@gmail.com>
2025-10-21 18:23:58 -0400

    Configuring Google Provider

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotFoundPage = () => {
   return (
-    <div>How the pog did you get here bro? This shit do&apos;t exist.</div>
+    <div>How the pog did you get here bro? This shit don&apos;t exist.</div>
   );
 };
 

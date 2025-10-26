8b06acf8fd4d74425fc7855df4e71f2f449250f5
Krishna Nagar <notkrises@gmail.com>
2025-10-26 18:34:17 -0400

    Accessing Sessions on the Client

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import ProductCard from "../components/custom/ProductCard/ProductCard";
 import { ThemeToggle } from "@/components/custom/theme/ThemeToggle";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
       <ThemeToggle />
-      <h1>Hello World</h1>
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
       <ProductCard />
 
